f16fa032876a15cd9b5aa15f989b71202db85f42
Deepanshu <work.deepanshurawat@gmail.com>
2025-07-24 23:43:53 +0530

    work in progress

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def main():
+def index():
     return render_template('index.html')
 
 
 @app.route("/data")
-def main():
-    return "Database"
+def show_database():
+    return render_template('data.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
